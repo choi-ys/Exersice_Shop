4f54cb93d2c09589744588585234eb1d7caa8164
choy-ys <project.log.062@gmail.com>
2021-03-22 10:20:14 +0900

    [2021-03-22]용석:Exercise Project Initial Commit

---
+++ src/main/java/io/exercise/shop/ShopApplication.java
@@ -0,0 +1,16 @@
+package io.exercise.shop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+        log.info("Initial Springboot Project is run .. ");
+    }
+
+}
